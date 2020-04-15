cf0930e614e2948879c360c201393592f29a20fe
zetsubou24 <alphabetaomega4@gmail.com>
2020-04-15 11:14:23 +0530

    Optimize Queries

--- SQL_Query_Assignment1/Query3.sql
+++ SQL_Query_Assignment1/Query3.sql
@@ -3,8 +3,7 @@ SELECT customer.customer_id, customer.first_name, customer.last_name, count( cus
 FROM customer
 JOIN rental ON customer.customer_id = rental.customer_id
 JOIN inventory ON rental.inventory_id = inventory.inventory_id
-JOIN film ON inventory.film_id = film.film_id
-JOIN film_category ON film.film_id = film_category.film_id
+JOIN film_category ON inventory.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
 JOIN address ON customer.address_id = address.address_id
 JOIN city ON address.city_id = city.city_id
--- SQL_Query_Assignment1/Query4.sql
+++ SQL_Query_Assignment1/Query4.sql
@@ -3,8 +3,7 @@ SELECT customer.customer_id, customer.first_name, customer.last_name, COUNT(cust
 FROM customer
 JOIN rental ON customer.customer_id = rental.customer_id
 JOIN inventory ON rental.inventory_id = inventory.inventory_id
-JOIN film ON inventory.film_id = film.film_id
-JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN film_actor ON inventory.film_id = film_actor.film_id
 JOIN actor ON film_actor.actor_id = actor.actor_id
 JOIN address ON customer.address_id = address.address_id
 JOIN city ON address.city_id = city.city_id
